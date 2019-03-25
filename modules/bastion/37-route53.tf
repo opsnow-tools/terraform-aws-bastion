5b48b2c02e800465fbc7d035e6100df3d175f87b
Jungyoul Yu <jungyoul.yu@bespinglobal.com>
2019-03-25 17:11:17 +0900

    Update 37-route53.tf

--- modules/bastion/37-route53.tf
+++ modules/bastion/37-route53.tf
@@ -1,16 +1,16 @@
-data "aws_route53_zone" "this" {
+data "aws_route53_zone" "default" {
   count = "${var.base_domain != "" ? 1 : 0}"
   name  = "${var.base_domain}"
 }
 
-resource "aws_route53_record" "this" {
+resource "aws_route53_record" "default" {
   count   = "${var.base_domain != "" ? 1 : 0}"
-  zone_id = "${data.aws_route53_zone.this.zone_id}"
-  name    = "${local.lower_name}.${data.aws_route53_zone.this.name}"
+  zone_id = "${data.aws_route53_zone.default.zone_id}"
+  name    = "${local.lower_name}.${data.aws_route53_zone.default.name}"
   type    = "A"
   ttl     = 300
 
   records = [
-    "${aws_eip.this.public_ip}",
+    "${aws_eip.default.public_ip}",
   ]
 }
